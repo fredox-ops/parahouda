1b82a5f2b97f04c0ddfd6adbfe5da9a5a022f57a
Mohammed Arousse <mohamedarousse5@gmail.com>
2025-03-13 21:12:09 +0000

    update

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInita54eebba16dd11e0ec77988b6d77ae69
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInita54eebba16dd11e0ec77988b6d77ae69', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInita54eebba16dd11e0ec77988b6d77ae69', 'loadClassLoader'));
